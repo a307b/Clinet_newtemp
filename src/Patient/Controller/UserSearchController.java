42315d808022622dcfdc56faafd0ffeabed10f60
sava17 <37406543+sava17@users.noreply.github.com>
2018-05-25 15:03:44 +0200

    Anden privatekylokalition, dog ikke relativ

--- src/Patient/Controller/UserSearchController.java
+++ src/Patient/Controller/UserSearchController.java
@@ -33,7 +33,7 @@ import java.util.stream.Collectors;
 public class UserSearchController implements Initializable
 {
     private PrivateKey privateKey;
-    private String privateKeyLocation = "C:\\GitHub\\Clinet_newtemp\\src\\privateKeys\\";
+    private String privateKeyLocation = "C:\\GitHub\\PatientClient\\src\\privateKeys";
     /* The public key is needed as both an String and publicKey throughout the program. To avoid unnecessary typecasting
      * which if done carelessly might result in padding-errors, there exist an variable for both. */
     private PublicKey publicKey;
@@ -57,7 +57,7 @@ public class UserSearchController implements Initializable
     public void CPRButtonAction(ActionEvent event)
     {
         String cprString = cprTextField.getText();
-/*
+
         if (cprString.length() != 10)
         {
             Alert alert = new Alert(Alert.AlertType.ERROR);
@@ -67,7 +67,7 @@ public class UserSearchController implements Initializable
             alert.show();
             return;
         }
-*/
+
         if (!cprString.matches("[0-9]+"))
         {
             Alert alert = new Alert(Alert.AlertType.ERROR);
@@ -105,7 +105,7 @@ public class UserSearchController implements Initializable
             /* Reads and save the private key of the entered CPR */
             try {
                 /* Retrieves privateKey string from local file and converts it into an private key */
-                BufferedReader bufferedReader = new BufferedReader(Files.newBufferedReader(Paths.get(privateKeyLocation+ cprString +".txt")));
+                BufferedReader bufferedReader = new BufferedReader(Files.newBufferedReader(Paths.get(privateKeyLocation + "\\" + cprString +".txt")));
                 /* Saves the content of the file in privateKeyString */
                 String privateKeyString = bufferedReader.lines().collect(Collectors.joining());
                 System.out.println(privateKeyString);
